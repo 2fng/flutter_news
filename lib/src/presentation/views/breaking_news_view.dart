dff77d990dfe6c5a3ec66153507775386937e734
tunghs-1253 <hua.son.tung@sun-asterisk.com>
2023-05-17 16:16:49 +0700

    Setup router and constants

---
+++ lib/src/presentation/views/breaking_news_view.dart
@@ -0,0 +1,14 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+@RoutePage()
+class BreakingNewsView extends HookWidget {
+  const BreakingNewsView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Text('BreakingNewsView');
+  }
+}
---
+++ lib/src/utils/constants/strings.dart
@@ -0,0 +1,10 @@
+// App
+const String appTitle = 'Flutter Clean Architecture';
+
+// Networking and APIs
+const String baseUrl = 'https://newsapi.org/v2';
+const String defaultApiKey = 'c99388fdc7664dedaf487744dc72421a';
+
+// Storage and Databases
+const String articlesTableName = 'articales_table';
+const String databaseName = 'app_database.db';
